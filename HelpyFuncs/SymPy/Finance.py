1b9c76cb75afa1c118b822f149c78146d560e57d
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-02-17 11:15:29 -0600

    update SymPy Finance module

--- HelpyFuncs/SymPy/Finance.py
+++ HelpyFuncs/SymPy/Finance.py
@@ -1,15 +1,31 @@
-import sympy
-from . import sympy_vector_sum_elems
+from sympy.matrices import Determinant, Matrix
 
 
-def pv(amount=0., discount_rate=0., nb_periods=0):
+def terminal_value(
+        cash_flows=Matrix([0.]),
+        long_term_discount_rate=0.,
+        long_term_growth_rate=0.):
+    m, n = cash_flows.shape
+    if m == 1:
+        filter_vector = Matrix((n - 1) * [0] + [1])
+        tv = Determinant(cash_flows * filter_vector)
+    elif n == 1:
+        filter_vector = Matrix([(m - 1) * [0] + [1]])
+        tv = Determinant(filter_vector * cash_flows)
+    return (1 + long_term_growth_rate) * tv / (long_term_discount_rate - long_term_growth_rate)
+
+
+def present_value(amount=0., discount_rate=0., nb_periods=0.):
     return amount / (discount_rate ** nb_periods)
 
 
-def npv(cash_flows=sympy.Matrix([0.]), discount_rate=0.):
+def net_present_value(
+        cash_flows=Matrix([0.]),
+        discount_rate=0.):
     m, n = cash_flows.shape
     if m == 1:
-        discount_vector = sympy.Matrix([[discount_rate ** -i for i in range(n)]])
+        discount_vector = Matrix([discount_rate ** -i for i in range(n)])
+        return Determinant(cash_flows * discount_vector)
     elif n == 1:
-        discount_vector = sympy.Matrix([discount_rate ** -i for i in range(m)])
-    return sympy_vector_sum_elems(discount_vector.multiply_elementwise(cash_flows))
+        discount_vector = Matrix([[discount_rate ** -i for i in range(m)]])
+        return Determinant(discount_vector * cash_flows)
